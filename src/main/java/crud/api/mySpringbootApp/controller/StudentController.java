10767dddee1ef1abe95031da34ed236b4edab015
Jitendra Rawat <jitendra.rawat.15nov@gmail.com>
2024-02-16 16:56:16 -0500

    added firebase dependency, getAllStudents, getStudentById methods

--- src/main/java/crud/api/mySpringbootApp/controller/StudentController.java
+++ src/main/java/crud/api/mySpringbootApp/controller/StudentController.java
@@ -1,9 +1,17 @@
 package crud.api.mySpringbootApp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import crud.api.mySpringbootApp.exception.ResourceNotFoundException;
+import crud.api.mySpringbootApp.model.Student;
+import crud.api.mySpringbootApp.repository.StudentRepository;
 
 /**
  * The type Student controller.
@@ -15,9 +23,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class StudentController {
 
-    @GetMapping("/students")
-    public String home(){
-        return "Hello World!";
-    }
-    
+	@Autowired
+	private StudentRepository studentRepository;
+
+	/**
+	 * Get all students list.
+	 *
+	 * @return the list
+	 */
+	@GetMapping("/students")
+	public List<Student> getAllStudents() {
+		return studentRepository.findAll();
+	}
+
+	/**
+	 * Get student by id.
+	 *
+	 * @param studentId the student id
+	 * @return the students by id
+	 * @throws ResourceNotFoundException the resource not found exception
+	 */
+	@GetMapping("/students/{id}")
+	public ResponseEntity<Student> getUsersById(@PathVariable(value = "id") Long studentId) throws ResourceNotFoundException {
+		Student student = studentRepository.findById(studentId)
+				.orElseThrow(() -> new ResourceNotFoundException("Student not found on :: " + studentId));
+		return ResponseEntity.ok().body(student);
+	}
+
 }
---
+++ src/main/java/crud/api/mySpringbootApp/repository/StudentRepository.java
@@ -0,0 +1,15 @@
+package crud.api.mySpringbootApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import crud.api.mySpringbootApp.model.Student;
+
+
+/**
+ * The interface Student repository.
+ *
+ * @author Jitendra rawat
+ */
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
